7f28d02cc1e2ba3263f68d2bf53c891cae771c9f
Horko152 <voloshenyuk98@gmail.com>
2019-07-26 09:55:41 +0300

    Adedd Swagger settings

--- EShopWebApi/Startup.cs
+++ EShopWebApi/Startup.cs
@@ -11,6 +11,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
+
 
 namespace EShopWebApi
 {
@@ -31,6 +33,10 @@ namespace EShopWebApi
 			services.AddSingleton<EShopDbContext>();
 			services.AddSingleton<OrderRepository>();
 			services.AddSingleton<Order_ItemRepository>();
+			services.AddSwaggerGen(c =>
+			{
+				c.SwaggerDoc("v1", new Info { Title = "My API", Version = "v1" });
+			});
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -54,6 +60,14 @@ namespace EShopWebApi
 				routes.MapRoute(
 					name: "default",
 					template: "api/{controller}/{id?}");
+			});
+
+			app.UseSwagger();
+
+			// Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), specifying the Swagger JSON endpoint.  
+			app.UseSwaggerUI(c =>
+			{
+				c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
 			});
 		}
 	}
